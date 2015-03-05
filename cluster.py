32516f3ff3ba7ad019480150f62dff7de19b5130
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2015-03-05 14:25:26 +0100

    Make use of pair

--- cluster.py
+++ cluster.py
@@ -7,82 +7,35 @@ from mininet.topo import LinearTopo
 from mininet.log import setLogLevel
 from mininet.node import RemoteController
 from mininet.cli import CLI
-from sets import Set
-import random
-import string
 
 
-def setupIperfPair(client, server):
-    setupIperfServer(server)
-    setupIperfClient(client, server)
-    while not checkIperfClientCompleted(client):
-        pass
-    teardownIperfServer(server)
+def setupPair(pair):
+    (no, activeHost, passiveHost) = pair
+    activeCmd = formatPairCmd(activeHost, passiveHost, 'active', no)
+    passiveCmd = formatPairCmd(passiveHost, activeHost, 'passive', no)
+    passiveHost.cmd(passiveCmd)
+    activeHost.cmd(activeCmd)
 
 
-def setupIperfServer(host):
-    cmd = 'iperf -s > %s' % getIperfServerFilename(host)
-    host.sendCmd(cmd)
+def formatPairCmd(host, peer, state, no):
+    cmd = 'cd pair && make dev state={state} ip="{ip}" \
+    peer_ip="{peer_ip}" pair_no={no} it={it} intf={intf}'
+    return cmd.format(state=state,
+                      ip=host.IP(),
+                      peer_ip=peer.IP(),
+                      no=no,
+                      it=10,
+                      intf=host.intfs[0].str())
 
 
-def setupIperfClient(client, server):
-    clientCmd = 'iperf -c %s -t 1s' % (server.IP())
-    client.sendCmd(clientCmd)
-
-
-def checkIperfClientCompleted(client):
-    if client.waiting:
-        client.monitor(timeoutms=200)
-        if client.waiting:
-            return False
-    return True
-
-
-def teardownIperfServer(server):
-    server.sendInt()
-    server.waitOutput()
-
-
-def getIperfServerFilename(server):
-    return '%s-%s.iperf-srv' % (server.name, server.IP())
-
-
-def designateClientsAndServers(hosts):
-    servers = []
-    clients = []
+def designatePairs(hosts):
+    pairs = []
+    pair_no = 1
     for i in range(0, len(hosts) - 1, 2):
-        servers.append(hosts[i])
-        clients.append(hosts[i+1])
-    print "CLIENTS: %s" % string.translate(str(clients), None, "'")
-    print "SERVERS: %s" % string.translate(str(servers), None, "'")
-    return clients, servers
-
-
-def loop(runs, clients, servers):
-    freeClients = Set(clients)
-    runningClients = Set()
-    for r in range(0, runs):
-        while freeClients:
-            c = freeClients.pop()
-            runningClients.add(c)
-            s = servers[random.randint(0, len(servers) - 1)]
-            print "Running: %s:%s -> %s:%s" % (c.name, c.IP(), s.name, s.IP())
-            setupIperfClient(c, s)
-        freeClients = findFreeClients(clients)
-    for c in clients:
-        while not checkIperfClientCompleted(c):
-            pass
-
-
-def findFreeClients(clients):
-    free = Set()
-    while not free:
-        for c in clients:
-            if not c.waiting:
-                free.add(c)
-            else:
-                c.monitor(timeoutms=300)
-    return free
+        pair = (pair_no, hosts([i]), hosts[i+1])
+        pairs.append(pair)
+        pair_no += 1
+    return pairs
 
 
 def run():
@@ -91,18 +44,11 @@ def run():
     controller = RemoteController('c0', ip='192.168.56.1', port=6653)
     net = MininetCluster(topo=topo, servers=servers, controller=controller)
     net.start()
-    clients, servers = designateClientsAndServers(net.hosts)
-    [setupIperfServer(s) for s in servers]
-    loop(50, clients, servers)
-    [teardownIperfServer(s) for s in servers]
-    # h1 = net.hosts[0]
-    # h2 = net.hosts[1]
-    # setupIperfPair(h1,h2)
-    # setupIperfPair(h2,h1)
+    pairs = designatePairs(net.hosts)
+    [setupPair(p) for p in pairs]
     CLI(net)
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')
     run()
-
