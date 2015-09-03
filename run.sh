e9d50bc4947aeb8a2380d6a8189873c02ef66c0b
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2015-09-03 14:15:11 +0200

    Add run script

---
+++ run.sh
@@ -0,0 +1,34 @@
+Models=(proc_per_switch regular)
+Cores=(1 2)
+# Hosts=(10 20 50 100)
+Hosts=(10 20 50)
+Hosts2=(100 200)
+
+for m in ${Models[*]}; do 
+    for c in ${Cores[*]}; do
+        for h in ${Hosts[*]}; do
+            sudo -E ./cluster.py \
+            --mn-hosts localhost mn2 mn3 \
+            --hosts $h \
+            --switches 4 \
+            --iterations 1000 \
+            --mode $m \
+            --schedulers-online $c 
+        done;
+    done
+done
+
+
+for m in ${Models[*]}; do 
+    for c in ${Cores[*]}; do
+        for h in ${Hosts2[*]}; do
+            sudo -E ./cluster.py \
+            --mn-hosts localhost mn2 mn3 \
+            --hosts $h \
+            --switches 4 \
+            --iterations 1000 \
+            --mode $m \
+            --schedulers-online $c 
+        done;
+    done
+done
